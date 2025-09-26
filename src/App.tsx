d4529a2945668df7f695b6f3391aac30286cd93b
georgeeduardtp <georgeeduard.tp@gmail.com>
2025-09-26 10:59:37 +0200

    creamos artistCard

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "@/screens/Home";
+import PageNotFound from "@/screens/PageNotFound";
 
 export default function App() {  //Este código configura el sistema de enrutamiento principal para una 
   return (                       // aplicación web hecha con React. Su función es mostrar la página de inicio (Home) cuando un usuario visita la raíz del sitio web.
     <HashRouter>
       <Routes>  
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />   {/*//La Ruta Raíz o Principal*/}
+        <Route path="*" element={<PageNotFound />} /> {/*//(La Ruta "Catch-All" o Comodín)*/}
       </Routes>
     </HashRouter>
   );
